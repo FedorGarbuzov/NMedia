e0c51c3e4ab6d9e89b2bb07c457c64dfdb08d05c
Fedor Garbuzov <71636417+FedorGarbuzov@users.noreply.github.com>
2021-06-15 12:02:49 +0300

    Singletons

---
+++ app/src/main/java/ru/netology/nmedia/application/FirebaseModule.kt
@@ -0,0 +1,18 @@
+package ru.netology.nmedia.application
+
+import com.google.firebase.messaging.FirebaseMessaging
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object FirebaseModule {
+    @Provides
+    @Singleton
+    fun provideFirebase(): FirebaseMessaging {
+        return FirebaseMessaging.getInstance()
+    }
+}
