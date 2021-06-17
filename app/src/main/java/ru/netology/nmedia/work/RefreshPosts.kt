35a6f3beb5037799b6ffabed91eb356a764bea7e
Fedor Garbuzov <71636417+FedorGarbuzov@users.noreply.github.com>
2021-06-17 10:21:43 +0300

    Hilt & WorkManager

--- app/src/main/java/ru/netology/nmedia/work/RefreshPosts.kt
+++ app/src/main/java/ru/netology/nmedia/work/RefreshPosts.kt
@@ -1,19 +1,23 @@
 package ru.netology.nmedia.work
 
 import android.content.Context
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.ListenableWorker
 import androidx.work.WorkerFactory
 import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ru.netology.nmedia.repository.post.PostRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class RefreshPostsWorker(
-    applicationContext: Context,
-    params: WorkerParameters,
+@HiltWorker
+class RefreshPostsWorker @AssistedInject constructor(
+    @Assisted applicationContext: Context,
+    @Assisted params: WorkerParameters,
     private val repository: PostRepository
 ) : CoroutineWorker(applicationContext, params) {
     companion object {
--- app/src/main/java/ru/netology/nmedia/work/RemovePost.kt
+++ app/src/main/java/ru/netology/nmedia/work/RemovePost.kt
@@ -1,19 +1,23 @@
 package ru.netology.nmedia.work
 
 import android.content.Context
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.ListenableWorker
 import androidx.work.WorkerFactory
 import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import ru.netology.nmedia.repository.post.PostRepository
 import ru.netology.nmedia.work.SavePostsWorker.Companion.postKey
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class RemovePostsWorker(
-        applicationContext: Context,
-        params: WorkerParameters,
-        private val repository: PostRepository
+@HiltWorker
+class RemovePostsWorker @AssistedInject constructor(
+    @Assisted applicationContext: Context,
+    @Assisted params: WorkerParameters,
+    private val repository: PostRepository
 ) : CoroutineWorker(applicationContext, params) {
 
     override suspend fun doWork(): Result {
